18738b621fab731fe34f3cef36d3c459f3ded130
Brian Wynn <apple.com>
2015-04-24 13:18:08 -0700

    Wrapped up the chapter with an example of a syntax that could be used to enable the subclassing of objects, and then created a method to implement.

--- ch6_object-orientation-with-prototypes/script22.js
+++ ch6_object-orientation-with-prototypes/script22.js
@@ -5,3 +5,56 @@ function assert(value, desc) {
   li.appendChild(document.createTextNode(desc));
   document.getElementById('results').appendChild(li);
 }
+
+(function() {
+  var initializing = false,
+  // this gnarly regex determines if functions can be serialized.
+      superPattern = /xyz/.test(function() { xyz; }) ? /\b_super\b/ : /.*/;
+
+  // adds a subClass method to the Object
+  Object.subClass = function(properties) {
+    var _super = this.prototype;
+
+    // instantiates the superclass
+    initializing = true;
+    var proto = new this();
+    initializing = false;
+
+    for (var name in properties) {
+      proto[name] = typeof properties[name] == 'function' &&
+                    typeof _super[name] == 'function' &&
+                    superPattern.test(properties[name]) ?
+                    // defines an overriding function
+                    (function(name, fn) {
+                      return function() {
+                        var tmp = this._super;
+
+                        this._super = _super[name];
+
+                        var ret = fn.apply(this, arguments);
+                        this._super = tmp;
+
+                        return ret;
+                      };
+                    })(name, properties[name]) :
+                    properties[name];
+    }
+
+    function Class() {
+      // all construction is actually done in the init method
+      if (!initializing && this.init)
+      this.init.apply(this, arguments);
+    }
+
+    // populates the class prototype
+    Class.prototype = proto;
+
+    // overrides the constructor reference
+    Class.constructor = Class;
+
+    // makes the class extendable
+    Class.subClass = arguments.callee;
+
+    return Class;
+  };
+})();
